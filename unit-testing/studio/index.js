ad3bd39044601a317611b7a369a4a666ddd5a7bd
Mango <greatdogsite@gmail.com>
2023-10-15 21:45:04 -0500

    working through student content

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,31 @@
 
-let launchcode = {
-  
+function launchOutput(num){
+    let string = "";
+    if (num%2===0){string += "Launch"}
+    if (num%3===0){string += "Code"}
+    if (num%5===0){string += string.length === 0 ? "Rocks" : " Rocks";}
+    if (string.length===0) {return "Rutabagas! That doesn't work.";}
+    return string + "!";
 }
+
+let launchcode = {
+
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    method: launchOutput
+}
+
+
+console.log(launchOutput(2));
+console.log(launchOutput(3));
+console.log(launchOutput(5));
+console.log(launchOutput(6));
+console.log(launchOutput(15));
+console.log(launchOutput(10));
+console.log(launchOutput(30));
+console.log(launchOutput(31));
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,41 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  it("checks if lc is nonprofit", function() {
+		let string = launchcode.organization
+		expect(string).toEqual("nonprofit");
+	});
+  
+  it("checks if jeff is the executive director", function() {
+		let string = launchcode.executiveDirector
+		expect(string).toEqual("Jeff");
+	});
+
+  it("checks if percentageCoolEmployees to be 100", function() {
+		let integer = launchcode.percentageCoolEmployees
+		expect(integer).toEqual(100);
+	});
+
+  it("checks the values of programsOffered", function() {
+		let array = launchcode.programsOffered
+		expect(array[0]).toEqual("Web Development");
+    expect(array[1]).toEqual("Data Analysis");
+    expect(array[2]).toEqual("Liftoff");
+    expect(array.length).toEqual(3)
+	});
+
+  it("checks that the correct things are printed based on the number given", function() {
+		let objectFunction = launchcode.method;
+		expect(objectFunction(2)).toEqual("Launch!");
+    expect(objectFunction(3)).toEqual("Code!");
+    expect(objectFunction(5)).toEqual("Rocks!");
+    expect(objectFunction(6)).toEqual("LaunchCode!");
+    expect(objectFunction(15)).toEqual("Code Rocks!");
+    expect(objectFunction(10)).toEqual("Launch Rocks!");
+    expect(objectFunction(30)).toEqual("LaunchCode Rocks!");
+    expect(objectFunction(31)).toEqual("Rutabagas! That doesn't work.");
+
+	});
   
 });
+
